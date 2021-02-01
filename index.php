c899f691fd47b5004c126876e79c01b12ff4a33a
KochevDmitry <65921394+KochevDmitry@users.noreply.github.com>
2021-02-01 22:07:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,18 @@
     </head>
     <body>
         <h1 align="center">Hello</h1>
+        <hr>
         <h3>Make new comment</h3>
         <form name=form action="table.php" method=POST>
             <p>Surname</p>
-            <input type=text name=surname>
+            <input type=text name=surname required>
             <p>Name</p>
-            <input type=text name=name>
+            <input type=text name=name required>
             <p>Your comment</p>
-            <textarea name="comment" class="form-control" id="comtextarea"></textarea>
-            <input type=submit value = 'Comment'>
+            <textarea name="comment" class="form-control" id="comtextarea" required></textarea>
+            <input type=submit value = 'Make comment'>
         </form>
+        <hr>
+        <h2><a href="table.php">Check last comments</a></h2>
     </body>
 </html>
